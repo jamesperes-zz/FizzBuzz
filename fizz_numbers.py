3be7a75472921cbe2bf0ea93e4c7803c0d744348
James Peres <james.peres.s@gmail.com>
2018-05-31 02:17:38 +0100

    refactor

--- fizz_numbers.py
+++ fizz_numbers.py
@@ -1,6 +1,17 @@
-def fizz_numbers(value):
+def fizz_numbers(list_values):
+    """Do a FizzBuzz with numbers where multiples of three change to “Three”
+    instead of the number and for the multiples of five change to “Five” and
+    for numbers which are multiples of both three and five change to
+    “ThreeFive”.
+
+    Keyword arguments:
+    list_values -- list containing only numbers
+
+    Return:
+    list containing str and int
+    """
     result = []
-    for v in value:
+    for v in list_values:
         if v % 3 == 0 and  v % 5 == 0:
             result.append('ThreeFive')
         elif v % 3 == 0:
@@ -11,3 +22,7 @@ def fizz_numbers(value):
             result.append(v)
 
     return result
+
+
+if __name__ == '__main__':
+    print(fizz_numbers(range(1,101)))
--- test_fizz_numbers.py
+++ test_fizz_numbers.py
@@ -11,12 +11,15 @@ class FizzNumbersTest(unittest.TestCase):
         self.assertEqual(fizz_numbers(list_two_values), [1, 2])
 
     def test_receive_first_number_tree(self):
+        self.assertTrue('Three' in fizz_numbers(range(1,4)))
         self.assertEqual(fizz_numbers(range(1,4))[2], 'Three')
 
     def test_receive_first_number_five(self):
+        self.assertTrue('Five' in fizz_numbers(range(1,6)))
         self.assertEqual(fizz_numbers(range(1,6))[4], 'Five')
 
     def test_reveive_first_number_treefive(self):
+        self.assertTrue('ThreeFive' in fizz_numbers(range(1,16)))
         self.assertEqual(fizz_numbers(range(1,16))[14], 'ThreeFive')
 
     def test_get_last_number_tree(self):
